e7e2b947cb932bfc873722ab3cf83b4abb410d49
kristen-schneider <kristen.schneider@colorado.edu>
2022-03-04 21:26:15 -0700

    basic query skeleton set up

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,10 @@
 import generate_sample_major
 import parse_vcf
+import query_full
 
 vcf_file = 'data/easy.txt'
 num_samples = 3
-num_genotypes = 10
+num_variants = 10
 
 m_file = 'out/maternal.txt'
 p_file = 'out/paternal.txt'
@@ -11,10 +12,13 @@ o_file = 'out/out.txt'
 
 def main():
     # generate quick file with sample major genotypes
-    #generate_sample_major.write_genotypes(vcf_file, num_samples, num_genotypes)
+    #generate_sample_major.write_genotypes(vcf_file, num_samples, num_variants)
 
     # parse vcf file and encode genotypes
-    parse_vcf.parse_file(vcf_file, m_file, p_file, o_file)
+    #parse_vcf.parse_file(vcf_file, m_file, p_file, o_file)
+
+    # query
+    query_full.homo_ref(o_file, num_samples, num_variants)
 
 
 
